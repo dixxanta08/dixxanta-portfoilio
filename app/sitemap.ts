82fcf4e49a352313d7b03998fd8883588d979319
dixxanta08 <dixantashrestha08@gmail.com>
2025-12-23 07:53:11 +0545

    sitemap updateD

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,15 @@
 import type { MetadataRoute } from "next";
+import blogs from "./data/blogs.json";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://www.dixantashrestha.com.np";
+
+  const blogEntries = blogs.map((blog) => ({
+    url: `${baseUrl}/blogs/${blog.slug}`,
+    lastModified: new Date(blog.publishedDate),
+    changeFrequency: "monthly" as const,
+    priority: 0.7,
+  }));
 
   return [
     {
@@ -22,5 +30,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    ...blogEntries,
   ];
 }
